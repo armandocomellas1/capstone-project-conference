ea0d6339081a0a811380319d4baa83fe6d1b4738
GameMaster <businesswitharmando@gmail.com>
2022-07-25 21:00:18 -0500

    Add all code and array data of the mobile version home page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,70 @@
-//Comment
+let clickMenu = document.getElementById('menu_expand');
+
+clickMenu.addEventListener('click', function() {
+  let storeMenu = document.getElementById('menu_mobile');
+  storeMenu.getElementsByTagName('a')[0].style.visibility = "visible";
+  storeMenu.getElementsByTagName('a')[1].style.visibility = "visible";
+  document.getElementsByClassName('icon_menu')[0].style.visibility = "hidden";
+  document.getElementsByClassName('hide_menu')[0].style.visibility = "visible";
+})
+
+let hideMenu = document.getElementById('close_menu');
+
+hideMenu.addEventListener('click', function() {
+  document.getElementsByClassName('icon_menu')[0].style.visibility = "visible";
+  let storeMenu = document.getElementById('menu_mobile');
+  storeMenu.getElementsByTagName('a')[0].style.visibility = "hidden";
+  storeMenu.getElementsByTagName('a')[1].style.visibility = "hidden";
+  document.getElementsByClassName('icon_menu')[0].style.visibility = "visible";
+  document.getElementsByClassName('hide_menu')[0].style.visibility = "hidden";
+})
+
+//This part is dynamically for insert featured HTML and insert data
+
+let arrFeature = [
+  {Title: 'Featured Speakers'},
+  {
+    speakerOne: 'Yochai Benkler',
+    speakerSubtitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    speakerParagraph: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.'
+  },
+  {
+    speakerOne: 'SohYeong Noh',
+    speakerSubtitle: 'Director of Art Centre Nabi and a board member of CC Korea',
+    speakerParagraph: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanitites, and the arts.'
+  },
+  {button: 'MORE'}
+]
+
+for (var i = 0; i < 2; i++) {
+
+  let featureSection =
+  `<h2 class="speakers_title">${arrFeature[0].Title}</h2>
+  <hr class="section_title_deco">
+  <div class="first_speaker">
+    <div class="img_speaker_one"></div>
+    <article class="article_speaker_one">
+      <h3 class="first_speak_title">${arrFeature[1].speakerOne}</h3>
+      <p class="first_speak_subti">${arrFeature[1].speakerSubtitle}</p>
+      <p class="first_speak_para">${arrFeature[1].speakerParagraph}</p>
+    </article>
+  </div>
+  <div class="first_speaker">
+    <div class="img_speaker_two"></div>
+    <article class="article_speaker_one">
+      <h3 class="first_speak_title">${arrFeature[2].speakerOne}</h3>
+      <p class="first_speak_subti">${arrFeature[2].speakerSubtitle}</p>
+      <p class="first_speak_para">${arrFeature[2].speakerParagraph}</p>
+    </article>
+  </div>
+  <button type="button" class="final_section">
+    <div class="see_more">
+      <div class="final_sect_Text">${arrFeature[3].button}</div>
+      <div class="final_sect_icon"></div>
+    </div>
+  </button>`;
+
+  let insertFeatureData = document.getElementById('featured_section');
+  insertFeatureData.innerHTML = featureSection;
+
+}
